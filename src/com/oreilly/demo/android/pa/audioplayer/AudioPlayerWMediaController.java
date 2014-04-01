1384071cef4102b2f1b2ee5e6e88985bcb4e18d1
G. Blake Meike <blake.meike@gmail.com>
2014-04-01 12:17:09 -0700

    Add new method required for API Level-18 and greater

--- src/com/oreilly/demo/android/pa/audioplayer/AudioPlayerWMediaController.java
+++ src/com/oreilly/demo/android/pa/audioplayer/AudioPlayerWMediaController.java
@@ -8,134 +8,138 @@ import android.view.MotionEvent;
 import android.widget.MediaController;
 
 public class AudioPlayerWMediaController extends Activity {
-	public static final String AUDIOFILEURI		= "AudioFileUri";
+    public static final String AUDIOFILEURI        = "AudioFileUri";
 
-	private MediaPlayer mediaplayer;
-	private Uri mediaReference;
-	private MediaController controller;
-	private boolean begin = true;
+    private MediaPlayer mediaplayer;
+    private Uri mediaReference;
+    private MediaController controller;
+    private boolean begin = true;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		if(getIntent() != null && getIntent().hasExtra(AUDIOFILEURI))
-			mediaReference = getIntent().getParcelableExtra(AUDIOFILEURI);
+        if(getIntent() != null && getIntent().hasExtra(AUDIOFILEURI))
+            mediaReference = getIntent().getParcelableExtra(AUDIOFILEURI);
 
-		setUpView();
+        setUpView();
 
-		initializeMediaPlayer();
-	}
+        initializeMediaPlayer();
+    }
 
-	@Override
-	public void onWindowFocusChanged(boolean hasFocus) {
-		super.onWindowFocusChanged(hasFocus);
-		if(hasFocus && begin && !controller.isShowing()) {
-			begin = false;
-			controller.show(0);
-			mediaplayer.start();
-		}
-	}
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if(hasFocus && begin && !controller.isShowing()) {
+            begin = false;
+            controller.show(0);
+            mediaplayer.start();
+        }
+    }
 
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		mediaplayer.release();
-	}
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mediaplayer.release();
+    }
 
-	/////////////////////
-	// Set up the view //
-	/////////////////////
-	private void setUpView() {
-		setContentView(R.layout.audioplayerwmediacontrol);
-	}
+    /////////////////////
+    // Set up the view //
+    /////////////////////
+    private void setUpView() {
+        setContentView(R.layout.audioplayerwmediacontrol);
+    }
 
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		controller.show(0);
-		return super.onTouchEvent(event);
-	}
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        controller.show(0);
+        return super.onTouchEvent(event);
+    }
 
-	/////////////////////////////////
-	// MediaPlayer related methods //
-	/////////////////////////////////
-	private void initializeMediaPlayer() {
-		try {
-			if(mediaReference == null) {
-				mediaplayer = MediaPlayer.create(this, R.raw.example);
-			} else {
-				mediaplayer = new MediaPlayer();
-				mediaplayer.setDataSource(this, mediaReference);
-				mediaplayer.prepare();
-			}
+    /////////////////////////////////
+    // MediaPlayer related methods //
+    /////////////////////////////////
+    private void initializeMediaPlayer() {
+        try {
+            if(mediaReference == null) {
+                mediaplayer = MediaPlayer.create(this, R.raw.example);
+            } else {
+                mediaplayer = new MediaPlayer();
+                mediaplayer.setDataSource(this, mediaReference);
+                mediaplayer.prepare();
+            }
 
-			controller = new MediaController(this);
-			controller.setMediaPlayer(new AudioController(mediaplayer));
-			controller.setEnabled(true);
-			controller.setAnchorView(findViewById(R.id.mainlayout));
+            controller = new MediaController(this);
+            controller.setMediaPlayer(new AudioController(mediaplayer));
+            controller.setEnabled(true);
+            controller.setAnchorView(findViewById(R.id.mainlayout));
 
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static class AudioController implements MediaController.MediaPlayerControl {
-		private MediaPlayer media;
+    public static class AudioController implements MediaController.MediaPlayerControl {
+        private MediaPlayer media;
 
-		public AudioController(MediaPlayer md) {
-			media = md;
-		}
+        public AudioController(MediaPlayer md) {
+            media = md;
+        }
 
-		@Override
+        @Override
         public boolean canPause() {
-			return true;
-		}
+            return true;
+        }
 
-		@Override
+        @Override
         public boolean canSeekBackward() {
-			return true;
-		}
+            return true;
+        }
 
-		@Override
+        @Override
         public boolean canSeekForward() {
-			return true;
-		}
+            return true;
+        }
 
-		@Override
+        @Override
         public int getBufferPercentage() {
-			return 0;
-		}
+            return 0;
+        }
 
-		@Override
+        @Override
         public int getCurrentPosition() {
-			return media.getCurrentPosition();
-		}
+            return media.getCurrentPosition();
+        }
 
-		@Override
+        @Override
         public int getDuration() {
-			return media.getDuration();
-		}
+            return media.getDuration();
+        }
 
-		@Override
+        @Override
         public boolean isPlaying() {
-			return media.isPlaying();
-		}
+            return media.isPlaying();
+        }
 
-		@Override
+        @Override
         public void pause() {
-			media.pause();
-		}
+            media.pause();
+        }
 
-		@Override
+        @Override
         public void seekTo(int pos) {
-			media.seekTo(pos);
-		}
+            media.seekTo(pos);
+        }
 
-		@Override
+        @Override
         public void start() {
-			media.start();
-		}
+            media.start();
+        }
 
-	}
+        // Added to the API in Level 18
+        public int getAudioSessionId() {
+            throw new UnsupportedOperationException();
+        }
+    }
 }
